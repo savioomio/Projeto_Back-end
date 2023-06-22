06c91177d057e7ba5f9ff7e74157ac158ea7370d
savioomio <115905335+savioomio@users.noreply.github.com>
2023-06-22 20:23:24 -0300

    versão 1 de fazer login no sistema (concluído)

--- metodos/sis_cadastro_login/login.php
+++ metodos/sis_cadastro_login/login.php
@@ -1,22 +1,27 @@
-<?php
-require_once('conn.php');// inclui o arquivo de conexão
+<?php 
+require_once('conn.php');
 
-$email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL); // obtém o email informado pelo usuário e valida o formato
-$senha = $_POST['senha']; // obtém a senha informada pelo usuário
+$email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL); // Obtém e valida o email fornecido pelo usuário
+$senha = $_POST['senha']; // Obtém a senha fornecida pelo usuário
 
-$sql = 'SELECT * FROM usuario WHERE email=:email AND senha=:senha'; // define a query SQL para buscar um usuário com o email e senha informados
-$result = $conn->prepare($sql); // prepara a query SQL para execução
-$result->execute(['email' => $email, 'senha' => $senha]); // executa a query SQL, passando o email e senha como parâmetros
-$user = $result->fetch(); // armazena o resultado da query SQL em um array associativo
+$sql = 'SELECT * FROM usuario WHERE email=:email'; // Query SQL para buscar um usuário com o email informado
+$result = $conn->prepare($sql); // Prepara a query SQL para execução
+$result->execute(['email' => $email]); // Executa a query SQL, passando o email como parâmetro
+$user = $result->fetch(); // Armazena o resultado da query SQL em um array associativo
 
-if (!empty($user)){ // verifica se há resultados da query SQL
-    session_start(); // inicia uma sessão
+if (!empty($user)) {
+    $senhaCriptografada = substr(hash('sha256', $senha), 0, 32); // Criptografa a senha fornecida pelo usuário 
 
-    $_SESSION['username'] = $user['username']; // armazena o nome do usuário na sessão
-    
-    header('location: ../../pages/index.php'); // redireciona o usuário para a página inicial
-}else{ 
-    // caso não haja resultados da query SQL
-    echo '<script>alert("Aaaah bonitinho(a) rsrs. Tentando entrar mais colocou a senha ou email errado. Pode dar meia vouta e colocar tudo certinho!"); window.history.back();</script>';
-    exit(); // finaliza a execução do script
+    if ($senhaCriptografada === $user['senha']) { // Compara a senha criptografada com a senha armazenada no banco de dados
+        session_start();
+        $_SESSION['username'] = $user['username']; // Armazena o usernome do usuário na sessão
+        header('location: ../../pages/index.php'); // Redireciona o usuário para a página inicial
+    } else {
+        echo '<script>alert("Senha incorreta. Por favor, tente novamente."); window.history.back();</script>';
+        exit();
+    }
+} else {
+    echo '<script>alert("Email não encontrado. Por favor, tente novamente."); window.history.back();</script>';
+    exit();
 }
+
