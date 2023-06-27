7e7e68f3898155ec6fcb899186fc188e15869bf1
savioomio <115905335+savioomio@users.noreply.github.com>
2023-06-27 18:04:30 -0300

    atualizado arquivos

--- pages/index.php
+++ pages/index.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 session_start();
 require_once('../metodos/sis_cadastro_login/val_sessao.php');
@@ -32,4 +33,40 @@ validar_sessao('login.php');
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   </body>
+=======
+<?php
+session_start();
+require_once('../metodos/sis_cadastro_login/val_sessao.php');
+validar_sessao('login.php');
+
+?>
+
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <title>Pagina de principal</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+        
+   <div class="card text-center">
+     <div class="card-body">
+       <h4 class="card-title">Bem vindo!</h4>
+       <p class="card-text"><?php echo $_SESSION['username'];?></p>
+     </div>
+     <a href="../metodos/sis_cadastro_login/logout.php"><button type="button" class="btn btn-danger">Sair</button></a>
+   </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+>>>>>>> ac3909121e8916589188e976d5f192684314a439
 </html>
